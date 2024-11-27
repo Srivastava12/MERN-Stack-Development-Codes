9d11beb1b7924ea538ec6e403e5ae51ab4230a19
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-11-27 22:19:35 +0530

    Add files via upload

---
+++ Lecture-10/MathFunctions/app.js
@@ -0,0 +1,49 @@
+//Math Function's
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.LN2);
+// console.log(Math.sqrt(25));
+// console.log(Math.min(10,30,5,15,1));
+// console.log(Math.max(10,30,5,15,1));
+// console.log(Math.round(10.2));
+// console.log(Math.round(10.4));
+// console.log(Math.round(10.5));
+// console.log(Math.round(10.6));
+// console.log(Math.ceil(10.9999));
+// console.log(Math.floor(10.9999));
+
+//-------------------------------------------
+//Random
+//Default range 0-1
+//0.11111
+//0.0012
+//0.0123
+//0.1234
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*1000+1000));
+// console.log(Math.floor(Math.random()*6+22));
+
+// ==(General Equality) vs ===(Strict Equality)
+
+console.log(1==1);
+console.log(1===1);
+console.log(1=='1');
+console.log(1==='1');
+console.log(1==true);
+console.log(1===true);
+console.log(""==0);
+console.log(""===0);
+console.log([]==0);
+let ver=undefined;
+console.log(10===10.0);
+console.log(ver==0);
+
+// typeof();
+console.log(typeof(10.54));
+console.log(typeof(0/10));
+console.log(typeof(10/0));
+console.log(typeof(0/0));
+console.log(typeof(Infinity));
+console.log(typeof(undefined));
---
+++ Lecture-10/app.js
@@ -0,0 +1,39 @@
+//string
+// let userName='Shubham Srivastava 576';
+// let userName='Shubham\'s Srivastava';
+// let userName="Shubham's Srivastava";
+// console.log(userName);
+
+//----------------------------------------
+let firstName="Shubham";
+let lastName="Srivastava";
+// let space=" ";
+
+// console.log(firstName+space+lastName);
+// console.log(firstName+" "+lastName);
+let fullName=`My name is ${firstName} ${lastName}`;
+console.log(fullName);
+
+//---------------------------------------------------
+//Numbers 
+let a=10;
+let b=11.20;
+let c=-23;
+let d=13254545;
+let e=25;
+
+console.log(a+b);
+console.log(b-c);
+console.log(c*d);
+console.log(d/e);
+console.log(e%a);
+
+//Max-number infinity
+//min-number -infinity
+// JS operation on infinity numbers
+
+let max=Number.MAX_SAFE_INTEGER;
+let min=Number.MIN_SAFE_INTEGER;
+console.log(max);
+console.log(min);
+
